d57e350cc0ee3fda6d9ec47c64f1d7ad8b636855
rajanikanthch <110083488+rajanikanthch@users.noreply.github.com>
2023-11-07 23:54:12 +0530

    second commit

--- src/sample.test.js
+++ src/sample.test.js
@@ -1,13 +1,37 @@
 const { Builder, By, Key, until, Capabilities } = require("selenium-webdriver");
 
+var username = process.env.BROWSERSTACK_USERNAME;
+var accessKey = process.env.BROWSERSTACK_ACCESS_KEY;
+var buildName = process.env.BROWSERSTACK_BUILD_NAME;
+var browserstackLocal = process.env.BROWSERSTACK_LOCAL;
+var browserstackLocalIdentifier = process.env.BROWSERSTACK_LOCAL_IDENTIFIER;
+
+var capabilities = {
+	"os" : "Windows",
+	"os_version" : "10",
+	"browser" : "chrome",
+	"browser_version" : "latest",
+	"name": "BStack -[Jenkins] Sample Test", // test name
+	"build" : buildName, // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
+	"browserstack.local" : browserstackLocal,
+	"browserstack.localIdentifier" : browserstackLocalIdentifier,
+	"browserstack.user" : username,
+	"browserstack.key" : accessKey
+};
+
+
 describe("BStack demo test", () => {
   let driver;
 
   beforeAll(() => {
-    driver = new Builder()
-      .usingServer(`http://localhost:4444/wd/hub`)
-      .withCapabilities(Capabilities.chrome())
-      .build();
+    // driver = new Builder()
+    //   .usingServer(`http://localhost:4444/wd/hub`)
+    //   .withCapabilities(Capabilities.chrome())
+    //   .build();
+    driver = new Builder().
+      usingServer("https://hub-cloud.browserstack.com/wd/hub").
+      withCapabilities(capabilities).
+      build();
   });
   
   afterAll(async () => {
