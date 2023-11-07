d75f7c7ab9587c37e58e0b3c4f515c3102363dc5
rajanikanthch <110083488+rajanikanthch@users.noreply.github.com>
2023-11-08 00:00:02 +0530

    third commit

--- src/sample.test.js
+++ src/sample.test.js
@@ -2,7 +2,7 @@ const { Builder, By, Key, until, Capabilities } = require("selenium-webdriver");
 
 var username = process.env.BROWSERSTACK_USERNAME;
 var accessKey = process.env.BROWSERSTACK_ACCESS_KEY;
-var buildName = process.env.BROWSERSTACK_BUILD_NAME;
+// var buildName = process.env.BROWSERSTACK_BUILD_NAME;
 var browserstackLocal = process.env.BROWSERSTACK_LOCAL;
 var browserstackLocalIdentifier = process.env.BROWSERSTACK_LOCAL_IDENTIFIER;
 
@@ -12,7 +12,7 @@ var capabilities = {
 	"browser" : "chrome",
 	"browser_version" : "latest",
 	"name": "BStack -[Jenkins] Sample Test", // test name
-	"build" : buildName, // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
+	// "build" : buildName, // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
 	"browserstack.local" : browserstackLocal,
 	"browserstack.localIdentifier" : browserstackLocalIdentifier,
 	"browserstack.user" : username,
